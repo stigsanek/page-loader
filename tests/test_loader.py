1c2d1cb29a7787dbde9cb9edf3ad74e0e0ac2c19
stigsanek <stigsanek@yandex.ru>
2022-10-30 15:06:24 +0400

    Implements tests

---
+++ tests/test_loader.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+
+import pytest
+
+from page_loader.main import download
+
+FIXTURES = Path(__file__).parent / "fixtures"
+
+
+def get_file_data(file_path) -> str:
+    """
+    Get file data
+
+    :param file_path: file_path
+    :return: str
+    """
+    with open(file=file_path, encoding="utf-8") as f:
+        return f.read().strip()
+
+
+@pytest.mark.parametrize(
+    argnames="url, file",
+    argvalues=[
+        ("https://ru.hexlet.io/courses", "ru-hexlet-io-courses.html"),
+        ("https://www.php.net/docs.php", "www-php-net-docs.html")
+    ]
+)
+def test_download(url, file, tmp_path, requests_mock):
+    """
+    Test for download function
+
+    :param url: page url
+    :param file: file name
+    :param tmp_path: temp dir
+    :param requests_mock: requests_mock object
+    :return:
+    """
+    got = get_file_data(FIXTURES / file)
+    requests_mock.get(url, text=got)
+
+    test_dir = tmp_path / "test"
+    test_dir.mkdir()
+
+    result_file = download(page_url=url, out_dir=str(test_dir))
+    expected = get_file_data(result_file)
+
+    assert got == expected
